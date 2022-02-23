0a0d63af3daa9cf09946678741dd8a11a6810475
meng <meng@b-and-qchina.com>
2022-02-23 10:52:39 +0800

    修改写法

--- lib/StateWidget.d.ts
+++ lib/StateWidget.d.ts
@@ -5,14 +5,14 @@
 import React from 'react';
 import {StateSource} from './StateSource';
 
-export interface ViewData<T> {
+export type ViewData<T> = {
   data: T;
   has: boolean;
 }
 
-interface Props2<T> {
+type Props2<T> = {
   source: StateSource<T>;
   child: (data: ViewData<T>) => React.ReactElement;
 }
 
-export class StateWidget<T> = (props: Readonly<Props2<T>>) => React.ReactElement;
+export type StateWidget<T> = (props: Props2<T>) => React.ReactElement;
